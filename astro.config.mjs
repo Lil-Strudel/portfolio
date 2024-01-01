daf222b02be6026f8188b236054abaa3a9140806
Aaron Santo <42657503+Lil-Strudel@users.noreply.github.com>
2024-01-01 12:17:53 -0700

    feat: svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "svelte",
+      }),
+    ],
+  },
+  integrations: [tailwind(), svelte()],
 });
+
